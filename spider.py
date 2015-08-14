7ba786e7af72a1646700cdd4d2fa9494ee0be32c
mageia <yzg963@163.com>
2015-08-15 00:53:58 +0800

    Code refactoring

--- spider.py
+++ spider.py
@@ -8,53 +8,77 @@ import string
 import random
 import re
 from bs4 import BeautifulSoup
+import sys 
+reload(sys) 
+sys.setdefaultencoding("utf-8")
 
 hosturl = 'http://www.maiziedu.com'
 posturl = 'http://www.maiziedu.com/user/login/'
 
-cj = cookielib.LWPCookieJar()
-cookie_support = urllib2.HTTPCookieProcessor(cj)
-opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)
-
 def randomString(length):
     return ''.join(random.choice(string.letters + string.digits) for i in xrange(length))
 
-csrftoken = randomString(32)
-headers = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate',
-    'Connection': 'keep-alive',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.130 Safari/537.36',
-    'Referer': 'http://www.maiziedu.com/',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Cookie': 'csrftoken=' + csrftoken + ';',
-}
+def opener():
+    cj = cookielib.LWPCookieJar()
+    cookie_support = urllib2.HTTPCookieProcessor(cj)
+    opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)
+    return opener
+    
+def login(username, password):
+    csrftoken = randomString(32)
+    headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.130 Safari/537.36',
+        'Referer': 'http://www.maiziedu.com/',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Cookie': 'csrftoken=' + csrftoken + ';',
+    }
+    postData = {
+        'csrfmiddlewaretoken': csrftoken,
+        'account_l': username,
+        'password_l': password,
+    }
+    postData = urllib.urlencode(postData)
+    request = urllib2.Request(posturl, postData, headers)
+    text = opener().open(request).read()
+    user_center_url = eval(text)['url']
+    return user_center_url
 
-postData = {
-    'csrfmiddlewaretoken': csrftoken,
-    'account_l': '',
-    'password_l': '',
-}
+def getallcourse():
+    course_lst = []
+    mainpage = opener().open(hosturl).read()
+    soup = BeautifulSoup(mainpage, 'html.parser')
+    souplilst = soup.find(name='div', attrs={'class':'ind-cour-box'}).ul.findAll('li')
+    for item in souplilst:
+        course_lst.append(hosturl + item.a['href'])
+    return course_lst
 
-postData = urllib.urlencode(postData)
-request = urllib2.Request(posturl, postData, headers)
-response = opener.open(request)
-text = response.read()
-user_center_url = eval(text)['url']
 
-##########################
-course_url = 'http://www.maiziedu.com/course/python/'
-course_code = opener.open(course_url).read()
-course_lst = re.findall(r'(?<=lead-img">)[^>]+', course_code)
+def getlessons(url): 
+    href = []
+    #title = []
+    content = opener().open(url).read()
+    course_lst = re.findall(r'(?<=lead-img">)[^>]+', content)
+    
+    for item in course_lst:
+        href += re.findall(r'(?<=href=")[^"]+', item)
+        #title += re.findall(r'(?<=title=")[^"]+', item)
+    return href
+    
+def getsections(url):
+    content = opener().open(url).read()
+    soup = BeautifulSoup(content, 'html.parser')
+    playlist = soup.find(name='div', attrs={'id': 'playlist'}).ul.findAll('li')
+    for item in playlist:
+        print item.a['href'], item.a.text
+        print BeautifulSoup(opener().open(hosturl + item.a['href'])).find(name='video', attrs={'id': 'microohvideo'}).source['src']
 
-lst = []
-for item in course_lst:
-    tmp = re.findall(r'(?<=href=")[^"]+', item)
-    if tmp:
-        lst += tmp
-print len(lst)
-print lst
 
-soup = BeautifulSoup(content, 'html.parser')
-soup.source.get('src')
-
+if __name__ == '__main__':
+    login('', '')
+    #course_lst = getallcourse()
+    course_url = hosturl + '/course/python/'
+    lessons = getlessons(course_url)
+    getsections(hosturl+lessons[0])
